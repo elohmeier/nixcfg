c9f3cb67129daaadf94e424bfd401a95b0c279ae
Enno Richter <enno@nerdworks.de>
2024-11-04 05:30:25 +0100

    tabula-java: reduce closure size

--- packages/tabula-java/default.nix
+++ packages/tabula-java/default.nix
@@ -1,36 +1,73 @@
 {
-  lib,
-  maven,
   fetchFromGitHub,
+  jdk_headless,
+  jre_minimal,
+  lib,
   makeWrapper,
-  jre,
+  maven,
+  stdenvNoCC,
 }:
 
-maven.buildMavenPackage {
+let
   pname = "tabula-java";
   version = "1.0.5-unstable-2024-09-04";
 
-  src = fetchFromGitHub {
-    owner = "tabulapdf";
-    repo = "tabula-java";
-    rev = "5d91f1d733c4895d31854a641c152220f8c5f341";
-    hash = "sha256-Vy9m8XjmoG1PiELK7MXnEgJYy3Dr3HvdgP4minc9ENU=";
+  mvnPkg = maven.buildMavenPackage {
+    inherit pname version;
+
+    src = fetchFromGitHub {
+      owner = "tabulapdf";
+      repo = "tabula-java";
+      rev = "5d91f1d733c4895d31854a641c152220f8c5f341";
+      hash = "sha256-Vy9m8XjmoG1PiELK7MXnEgJYy3Dr3HvdgP4minc9ENU=";
+    };
+
+    mvnHash = "sha256-MZ9aIy2vrgoHa/Me0ju2uvoIFcJSVBzmRWnqGJlmULw=";
+    mvnParameters = "compile assembly:single -Dmaven.test.skip=true";
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/lib
+      cp target/tabula-1.0.6-SNAPSHOT-jar-with-dependencies.jar $out/lib/tabula.jar
+
+      runHook postInstall
+    '';
   };
 
-  mvnHash = "sha256-MZ9aIy2vrgoHa/Me0ju2uvoIFcJSVBzmRWnqGJlmULw=";
-  mvnParameters = "compile assembly:single -Dmaven.test.skip=true";
+  jre = jre_minimal.override {
+    jdk = jdk_headless;
+
+    # tabula uses the java/awt/geom/Rectangle2D class
+    # java.awt is included in java.desktop
+    modules = [ "java.desktop" ];
+  };
+in
+stdenvNoCC.mkDerivation {
+  inherit pname version;
+
+  dontUnpack = true;
 
   nativeBuildInputs = [ makeWrapper ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    makeWrapper ${jre}/bin/java tabula-java \
+      --add-flags "-cp ${mvnPkg}/lib/tabula.jar" \
+      --add-flags "technology.tabula.CommandLineApp"
+
+    # run a quick test to see if all required jre classes can be imported
+    ./tabula-java --help >/dev/null
+
+    runHook postBuild
+  '';
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/{bin,lib}
-    cp target/tabula-1.0.6-SNAPSHOT-jar-with-dependencies.jar $out/lib/tabula.jar
-
-    makeWrapper ${jre}/bin/java $out/bin/tabula-java \
-      --add-flags "-cp $out/lib/tabula.jar" \
-      --add-flags "technology.tabula.CommandLineApp"
+    mkdir -p $out/bin
+    cp tabula-java $out/bin/tabula-java
 
     runHook postInstall
   '';
