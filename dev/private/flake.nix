2e885ecd70f3d908b81e09386c289fe7cd242a74
Enno Richter <enno@nerdworks.de>
2025-03-01 05:07:05 +0100

    update dev flake

--- dev/private/flake.nix
+++ dev/private/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "nixcfg private inputs";
 
-  inputs.nixos-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixos-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
   inputs.treefmt-nix.inputs.nixpkgs.follows = "";
@@ -9,7 +9,6 @@
   inputs.pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
   inputs.pre-commit-hooks-nix.inputs.flake-compat.follows = "flake-compat";
   inputs.pre-commit-hooks-nix.inputs.nixpkgs.follows = "";
-  inputs.pre-commit-hooks-nix.inputs.nixpkgs-stable.follows = "";
 
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixos-stable";
